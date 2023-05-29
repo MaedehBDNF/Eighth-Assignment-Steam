3e98786094a26212a10397ca721f3e96737bd131
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-29 19:11:39 +0330

    Add RegisterDto class for make register Request

---
+++ src/main/java/Shared/Dto/RegisterDto.java
@@ -0,0 +1,33 @@
+package Shared.Dto;
+
+import java.time.LocalDate;
+
+public class RegisterDto {
+    public String username;
+    public String password;
+    public LocalDate dateOfBirth;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
