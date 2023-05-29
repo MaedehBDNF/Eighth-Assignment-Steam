f599840fa117776660769a820193f14f19b90aa5
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-29 19:15:01 +0330

    Add GameDto class for make findOneGame Request and download Request

---
+++ src/main/java/Shared/Dto/GameDto.java
@@ -0,0 +1,22 @@
+package Shared.Dto;
+
+public class GameDto {
+    public String userId;
+    public String gameId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+}
