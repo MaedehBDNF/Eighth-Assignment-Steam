4382069222bb076b777b41df4dd6bb7376d4117d
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-29 19:25:55 +0330

    Add UserEntity for transfer users' data to database

---
+++ src/main/java/Server/User/UserEntity.java
@@ -0,0 +1,52 @@
+package Server.User;
+
+import java.time.LocalDate;
+
+public class UserEntity {
+    private String id = "";
+    private String username = "";
+    private String password = "";
+    private LocalDate dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
