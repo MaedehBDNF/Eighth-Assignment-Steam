070008cee6ca8c5a894d12a9c2b850f7e1e32e97
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-29 20:05:38 +0330

    Complete ServerMain with multiThread and socket programming

--- src/main/java/Server/ServerMain.java
+++ src/main/java/Server/ServerMain.java
@@ -1,7 +1,24 @@
 package Server;
 
-public class ServerMain {
-    public static void main(String[] args) {
+import Server.Manager.Manager;
+import Server.Tables.CreateTables;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+        final int serverPort = 3000;
+        ServerSocket server = new ServerSocket(serverPort);
+        CreateTables createTables = new CreateTables();
+
+        while (true) {
+            Socket socket = server.accept();
+            System.out.println("Client connected.");
+            Manager service = new Manager(socket);
+            Thread t = new Thread(service);
+            t.start();
+        }
     }
 }
